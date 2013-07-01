73bcd7c7110a80cd568df4030d9bd87d7cb01433
Neal Barbaro <nealbarbaro@gmail.com>
2013-07-01 15:44:38 -0700

    finished refactoring

--- js/script.js
+++ js/script.js
@@ -4,25 +4,64 @@ $(document).ready(function(){
   var num_clicks = 0;
   var selected = [];
 
+  var is_current_selection_a_match = function() {
+    var img1src = selected[0].find('img').attr('src');
+    var img2src = selected[1].find('img').attr('src');
+    return (img1src == img2src);
+  }
+
+  var handle_win = function() {
+    alert('Good Job! You found all the matches in '+num_clicks+' clicks!');
+    window.location.href = window.location.href;
+  }
+
+  var is_game_over = function() {
+    return ($('.tile.matched').size() == tiles.length)
+  }
+
+  var is_two_selected = function() {
+    return (selected.length == 2);
+  }
+
+  var increment_click_counter = function() {
+    num_clicks++;
+  }
+
+  var activate_tile = function(tile) {
+    tile.addClass('active');
+    selected.push(tile);
+  }
+
+  var deactivate_tiles = function() {
+    $('.tile.active').removeClass('active');
+    selected = [];
+  };
+
+
   var handle_click = function() {
     var tile = $(this);
-    if (tile.hasClass('active')) {
+    if (tile.hasClass('active') || tile.hasClass('matched')) {
       return false;
     }
-    if (selected.length == 2) {
-      $('.tile.active').removeClass('active');
-      selected = [];
+    if (is_two_selected()) {
+      deactivate_tiles();
     }
-    $(this).addClass('active');
-    num_clicks++;
-    selected.push(tile);
-  };
+    increment_click_counter();
+    activate_tile(tile);
+    if (is_two_selected() && is_current_selection_a_match()) {
+      $.each(selected, function(index, matched_tile) {
+        matched_tile.addClass('matched');
+      });
+      if (is_game_over()) {
+        handle_win();
+      }
+    }
+  }
+
 
   $.each(tiles, function(index, tile) {
     var tile = $(tile);
     tile.on('click', handle_click);
   });
-
-
 
 });
